5a3f1b20db029ddf4c74b2b2984134490db42f50
Jackton Levy <jacktonbrigs@gmail.com>
2019-02-09 10:12:46 -0800

    1d and 2d pure elastic collisions

---
+++ 1D collisions.py
@@ -0,0 +1,82 @@
+from graphics import GraphWin, Rectangle, Point
+from time import sleep
+from random import randint
+
+size = 500
+half = size/2
+
+
+def window():
+    win = GraphWin("Collsions", size, size)
+    return win
+
+
+def start():
+
+    m1 = input("Mass 1: ")
+    m2 = input("Mass 2: ")
+
+    return m1, m2
+
+
+def rect():
+    mass = start()
+
+    m1 = int(mass[0])
+    m2 = int(mass[1])
+
+    rect1 = Rectangle(Point(50, half+m1), Point(50+m1*2, half-m1))
+    rect2 = Rectangle(Point(size-50, half+m2), Point(size-m2*2-50, half-m2))
+
+    rect1.setFill("Blue")
+    rect2.setFill("Red")
+
+    return rect1, rect2, size-100-m1*2-m2*2, m1, m2
+
+
+def anim(rect1, rect2, dist, mass1, mass2):
+
+    win = window()
+
+    rect1.draw(win)
+    rect2.draw(win)
+
+    v1 = 2
+    v2 = -2
+
+    u1 = v1
+    u2 = v2
+
+    m1 = mass1*2
+    m2 = mass2*2
+
+    distance = dist
+    left = 50
+    right = size-50
+
+    while True:
+
+        u1 = v1
+        u2 = v2
+
+        rect1.move(v1, 0)
+        rect2.move(v2, 0)
+        distance -= v1-v2
+        left += v1
+        right += v2
+        sleep(0.02)
+        if distance <= 0:
+            v1 = (m1-m2)/(m1+m2)*u1 + (2*m2)/(m1+m2)*u2
+            v2 = (2*m1)/(m1+m2)*u1 + (m2-m1)/(m1+m2)*u2
+
+        if left <= 0:
+            v1 *= -1
+
+        if right >= size:
+            v2 *= -1
+
+        print(v1)
+
+hold = rect()
+
+anim(hold[0], hold[1], hold[2], hold[3], hold[4])
---
+++ 2D collisions.py
@@ -0,0 +1,127 @@
+from graphics import GraphWin, Rectangle, Point
+from time import sleep
+import random
+
+
+class Collisions:
+
+    neg_list = [1, -1]
+    rand_list = range(3, 6)
+
+    def __init__(self):
+
+        self.size = 500
+        self.v1x = Collisions.neg_list[random.randint(0, 1)]*Collisions.rand_list[random.randint(0, 2)]*random.random()
+        self.v1y = Collisions.neg_list[random.randint(0, 1)]*Collisions.rand_list[random.randint(0, 2)]*random.random()
+        self.v2x = Collisions.neg_list[random.randint(0, 1)]*Collisions.rand_list[random.randint(0, 2)]*random.random()
+        self.v2y = Collisions.neg_list[random.randint(0, 1)]*Collisions.rand_list[random.randint(0, 2)]*random.random()
+        self.m1 = 0
+        self.m2 = 0
+
+    def window(self):
+
+        win = GraphWin("2D Collisions", self.size, self.size)
+
+        return win
+
+    def define(self):
+
+        m1 = int(input("Mass 1: "))
+        m2 = int(input("Mass 2: "))
+
+        self.m1 = m1
+        self.m2 = m2
+
+    def draw(self):
+
+        p1 = (self.v1x ** 2 + self.v1y ** 2) ** 0.5 * self.m1
+        p2 = (self.v2x ** 2 + self.v2y ** 2) ** 0.5 * self.m2
+        pt = p1 + p2
+
+        print("Total Momentum: {} kgm/s".format(pt))
+
+        win = Collisions.window(self)
+
+        h1 = random.randint(self.m1*2, self.size - self.m1*2)
+        h2 = random.randint(self.m2*2, self.size - self.m2*2)
+
+        distancex = self.size - 100 - self.m1*2 - self.m2*2
+        distancey = h2-h1-self.m1-self.m2
+
+        rect1 = Rectangle(Point(50, h1+self.m1), Point(50+self.m1*2, h1-self.m1))
+        rect1.setFill("Blue")
+        rect2 = Rectangle(Point(self.size-50, h2+self.m2), Point(self.size-50-self.m2*2, h2-self.m2))
+        rect2.setFill("Red")
+
+        left1 = 50
+        right1 = -self.size+50+self.m1*2
+        top1 = h1-self.m1
+        bottom1 = -self.size+h1+self.m1
+        left2 = self.size-50-self.m2*2
+        right2 = self.size-50
+        top2 = h2-self.m2
+        bottom2 = -self.size+h2+self.m2
+
+        rect1.draw(win)
+        rect2.draw(win)
+
+        while True:
+
+            u1x = self.v1x
+            u1y = self.v1y
+            u2x = self.v2x
+            u2y = self.v2y
+
+            if -2*self.m1-2*self.m2 <= distancex <= 0:
+                ycheck = True
+            else:
+                ycheck = False
+
+            if -2*self.m1-2*self.m2 <= distancey <= 0:
+                xcheck = True
+            else:
+                xcheck = False
+
+            rect1.move(self.v1x, self.v1y)
+            rect2.move(self.v2x, self.v2y)
+
+            right1 += self.v1x
+            left1 += self.v1x
+            top1 += self.v1y
+            bottom1 += self.v1y
+            right2 += self.v2x
+            left2 += self.v2x
+            top2 += self.v2y
+            bottom2 += self.v2y
+
+            distancex -= self.v1x-self.v2x
+            distancey -= self.v1y-self.v2y
+
+            if right1 >= 0 or left1 <= 0:
+                self.v1x *= -1
+
+            if right2 >= self.size or left2 <= 0:
+                self.v2x *= -1
+
+            if top1 <= 0 or bottom1 >= 0:
+                self.v1y *= -1
+
+            if top2 <= 0 or bottom2 >= 0:
+                self.v2y *= -1
+
+            if -2*self.m1-2*self.m2 <= distancey <= 0 and ycheck:
+                if -2*self.m1-2*self.m2 <= distancex <= 0:
+                    self.v1y = (self.m1-self.m2)/(self.m1+self.m2)*u1y + (2*self.m2)/(self.m1+self.m2)*u2y
+                    self.v2y = (self.m2-self.m1)/(self.m1+self.m2)*u2y + (2*self.m1)/(self.m1+self.m2)*u1y
+
+            if -2*self.m1-2*self.m2 <= distancex <= 0 and xcheck:
+                if -2*self.m1-2*self.m2 <= distancey <= 0:
+                    self.v1x = (self.m1-self.m2)/(self.m1+self.m2)*u1x + (2*self.m2)/(self.m1+self.m2)*u2x
+                    self.v2x = (self.m2-self.m1)/(self.m1+self.m2)*u2x + (2*self.m1)/(self.m1+self.m2)*u1x
+            sleep(0.01)
+
+
+start = Collisions()
+start.define()
+start.draw()
+
